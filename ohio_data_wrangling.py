2cb028bb44e1c82d16cd8f4bcfc9ddb67de39650
LukeJakielaszek <tug52339@temple.edu>
2020-04-27 08:59:13 -0700

    Standardized dates for ohio data

--- ohio_data_wrangling.py
+++ ohio_data_wrangling.py
@@ -24,6 +24,17 @@ ohio_data_link = "https://coronavirus.ohio.gov/static/COVIDSummaryData.csv"
 # output file
 out_filename = "data/ohio_data.csv"
 
+def convert_date(val):
+    month, day, year = val.split("/")
+    if(month == "3"):
+        month = "MARCH"
+    elif(month == "4"):
+        month = "APRIL"
+
+    date = month + " " + day + ", " + year
+
+    return date
+    
 # scrape the page contents for ohio executive orders
 def ohio_data():
     # get the csv
@@ -36,8 +47,25 @@ def ohio_data():
 
     # replace all null with N/A
     df = pd.read_csv(temp_file_name)
-    #df = df.fillna("N/A")
 
+    # drop totals at bottom
+    df = df.drop(df.index[-1])
+
+    # standardize onset date
+    for i,val in enumerate(df["Onset Date"]):
+        if(val != ""):
+            df["Onset Date"][i] = convert_date(val)
+
+    # standardize death date
+    for i,val in enumerate(df["Date Of Death"]):
+        if(str(val) != "nan" and val != "" and val != "Unknown"):
+            df["Date Of Death"][i] = convert_date(val)
+
+    # standardize admission date
+    for i,val in enumerate(df["Admission Date"]):
+        if(str(val) != "nan" and val != "" and val != "Unknown"):
+            df["Admission Date"][i] = convert_date(val)
+            
     # write it to csv
     df.to_csv(out_filename, index=False)
 
